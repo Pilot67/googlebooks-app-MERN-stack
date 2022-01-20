71c67770380d8a1c53376c81498e92b6c3b24827
Stuart <86697483+Pilot67@users.noreply.github.com>
2022-01-21 10:10:18 +1100

    fixed missing link to book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -47,13 +47,14 @@ const SearchBooks = () => {
       }
 
       const { items } = await response.json();
-
+      console.log(items)
       const bookData = items.map((book) => ({
         bookId: book.id,
         authors: book.volumeInfo.authors || ["No author to display"],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
+        title: book.volumeInfo.title || "No Title Availabel",
+        description: book.volumeInfo.description || "No Description Available",
         image: book.volumeInfo.imageLinks?.thumbnail || "",
+        link: book.volumeInfo.infoLink || "",
       }));
 
       setSearchedBooks(bookData);

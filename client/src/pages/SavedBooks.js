c34a09d9dd9f62938d9d2ddbf49470b4b203314e
Stuart <86697483+Pilot67@users.noreply.github.com>
2022-01-20 17:54:44 +1100

    added window refresh when deleting book

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,58 +6,32 @@ import {
   Card,
   Button,
 } from "react-bootstrap";
-import { useQuery , useMutation } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations"
+import { REMOVE_BOOK } from "../utils/mutations";
 
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
+
+
+
 const SavedBooks = () => {
-  const [removeBook, {error} ] = useMutation(REMOVE_BOOK)
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
   const { loading, data } = useQuery(QUERY_ME);
   const userData = data?.me || [];
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   
   const handleDeleteBook = async (bookId) => {
-    try{
-    console.log(bookId)
-    const {data} = await removeBook({variables: {bookId: bookId}});
-    console.log(data)
-
+    try {
+      const { data } = await removeBook({ variables: { bookId: bookId } });
+      removeBookId(bookId);
     } catch (error) {
       console.log(error);
     }
-
+    window.location.reload()
   };
   
-  
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     const response = await deleteBook(bookId, token);
-
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
-
-  //     const updatedUser = await response.json();
-  //     setUserData(updatedUser);
-  //     // upon success, remove book's id from localStorage
-  //     removeBookId(bookId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  // if data isn't here yet, say so
-  // if (!userDataLength) {
-  //   return <h2>LOADING...</h2>;
-  // }
 
   if (loading) {
     return (
